a63787b3d7b7485f6139fe1d35ee450712f8f593
bluetoro_dev <238170073+bluetorodev@users.noreply.github.com>
2025-11-28 12:48:15 +0530

    Add scrollbarHide utility function
    Added scrollbarHide utility function to hide scrollbars.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Scrollbar utilities
+export function scrollbarHide() {
+  return "scrollbar-hide [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+}
